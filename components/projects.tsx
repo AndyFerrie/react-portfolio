767b46a7994d10b1d4763fc2cf163a326d16c3dc
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2023-10-24 07:40:09 +0100

    Starts building projects component

---
+++ components/projects.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SectionDivider from "./section-divider";
+import SectionHeading from "./section-heading";
+
+export default function Projects() {
+    return (
+        <section>
+            <SectionHeading>My Projects</SectionHeading>
+            <div></div>
+        </section>
+    );
+}
