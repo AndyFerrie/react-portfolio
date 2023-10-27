7203bc9303c395c1ce63062fe81ae3ba39114c99
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2023-10-27 07:28:27 +0100

    Adds experience component

--- components/experience.tsx
+++ components/experience.tsx
@@ -13,13 +13,35 @@ export default function Experience() {
     return (
         <section id="experience">
             <SectionHeading>My experience</SectionHeading>
-            <VerticalTimeline>
+            <VerticalTimeline lineColor="">
                 {experiencesData.map((item, index) => (
                     <React.Fragment key={index}>
-                        <VerticalTimelineElement>
-                            <h3>{item.title}</h3>
-                            <p>{item.location}</p>
-                            <p>{item.description}</p>
+                        <VerticalTimelineElement
+                            contentStyle={{
+                                background: "#F3F4F6",
+                                boxShadow: "none",
+                                border: "1px solid rgba(0, 0, 0, 0.05)",
+                                textAlign: "left",
+                                padding: "1.3rem 2rem",
+                            }}
+                            contentArrowStyle={{
+                                borderRight: "0.4rem solid #9CA3AF",
+                            }}
+                            date={item.date}
+                            icon={item.icon}
+                            iconStyle={{
+                                background: "white",
+                                fontSize: "1.5rem,",
+                            }}
+                            visible={true}
+                        >
+                            <h3 className="font-semibold capitalize">
+                                {item.title}
+                            </h3>
+                            <p className="font-normal !mt-0">{item.location}</p>
+                            <p className="!mt-1 !font-normal text-gray-700">
+                                {item.description}
+                            </p>
                         </VerticalTimelineElement>
                     </React.Fragment>
                 ))}
