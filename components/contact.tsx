f6cb19cee37233937e3e4617ff3d5778921c6bdc
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2023-10-28 08:30:48 +0100

    Adds contact component

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,12 +1,34 @@
+"use client";
+
 import React from "react";
 import SectionHeading from "./section-heading";
 import { FaPaperPlane } from "react-icons/fa";
+import { useSectionInView } from "@/lib/hooks";
+import { motion } from "framer-motion";
 
 export default function Contact() {
+    const { ref } = useSectionInView("Contact", 0.5);
+
     return (
-        <section id="contact" className="mb-20 sm:mb-28 w-[min(100%,38rem)]">
+        <motion.section
+            ref={ref}
+            id="contact"
+            className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+            initial={{
+                opacity: 0,
+            }}
+            whileInView={{
+                opacity: 1,
+            }}
+            transition={{
+                duration: 1,
+            }}
+            viewport={{
+                once: true,
+            }}
+        >
             <SectionHeading>Contact me</SectionHeading>
-            <p className="text-gray-700 ">
+            <p className="text-gray-700 -mt-6">
                 Please contact me directly at{" "}
                 <a className="underline" href="mailto:example@email.com">
                     example@email.com
@@ -15,15 +37,23 @@ export default function Contact() {
             </p>
 
             <form className="mt-10 flex flex-col" action="">
-                <input className="h-14 rounded-lg borderBlack" type="email" />
-                <textarea className="h-52 my-3 rounded-lg borderBlack p-4"></textarea>
+                <input
+                    className="h-14 rounded-lg borderBlack px-4"
+                    type="email"
+                    placeholder="Your email"
+                />
+                <textarea
+                    className="h-52 my-3 rounded-lg borderBlack p-4"
+                    placeholder="Your message"
+                ></textarea>
                 <button
-                    className="flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all"
+                    className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105"
                     type="submit"
                 >
-                    Submit <FaPaperPlane />{" "}
+                    Submit{" "}
+                    <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
                 </button>
             </form>
-        </section>
+        </motion.section>
     );
 }
