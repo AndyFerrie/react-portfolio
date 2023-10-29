a55fea14715e0c6b63407584be5672a916cc205c
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2023-10-29 17:11:28 +0000

    Links contact form email to send email action

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -2,6 +2,8 @@
 
 import { Resend } from "resend";
 import { getErrorMessage, validateString } from "@/lib/utils";
+import ContactFormEmail from "@/email/contact-form-email";
+import React from "react";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -27,7 +29,10 @@ export const sendEmail = async (formData: FormData) => {
             to: "andy.ferrie@hotmail.com",
             subject: "Message from contact form",
             reply_to: senderEmail as string,
-            text: message as string,
+            react: React.createElement(ContactFormEmail, {
+                message: message as string,
+                senderEmail: senderEmail as string,
+            }),
         });
     } catch (error: unknown) {
         return {
