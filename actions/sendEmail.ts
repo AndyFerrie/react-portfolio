3df2d7fff816ca799f7d035fd7900a7c90fda89f
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2023-10-29 09:16:55 +0000

    Adds error handling to contact form

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,12 +1,12 @@
 "use server";
 
 import { Resend } from "resend";
-import { validateString } from "@/lib/utils";
+import { getErrorMessage, validateString } from "@/lib/utils";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (formData: FormData) => {
-    const senderEmail = formData.get("email");
+    const senderEmail = formData.get("senderEmail");
     const message = formData.get("message");
 
     if (!validateString(senderEmail, 500)) {
@@ -21,11 +21,17 @@ export const sendEmail = async (formData: FormData) => {
         };
     }
 
-    resend.emails.send({
-        from: "onboarding@resend.dev",
-        to: "andy.ferrie@hotmail.com",
-        subject: "Message from contact form",
-        reply_to: senderEmail as string,
-        text: message as string,
-    });
+    try {
+        await resend.emails.send({
+            from: "Contact Form <onboarding@resend.dev>",
+            to: "andy.ferrie@hotmail.com",
+            subject: "Message from contact form",
+            reply_to: senderEmail as string,
+            text: message as string,
+        });
+    } catch (error: unknown) {
+        return {
+            error: getErrorMessage(error),
+        };
+    }
 };
